796d905ae8f2a98629518b726f72acc7abfd440b
PoojaHiregoudar <YOUR@EMAIL.com>
2023-02-16 10:39:47 -0800

    Initial steps

---
+++ WhoAmI/WhoAmI/WhoAmIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAmIApp.swift
+//  WhoAmI
+//
+//  Created by Pooja Hiregoudar on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct WhoAmIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
